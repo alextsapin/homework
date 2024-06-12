c82ac62e9d88fc5b0f0fc59d0fb78fa0f9772753
alextsapin <96056751+alextsapin@users.noreply.github.com>
2024-06-12 16:08:08 +0200

    Fixed HW2

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -14,7 +14,7 @@ export type UserType = {
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends: UserType[]; // пропиши типизацию
 };
 
 export const HW2 = () => {
@@ -47,7 +47,7 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+    const filteredUsers = users.myFriends.filter(user => user.address.city === "Los Angeles");
     setCurrentUsers({ myFriends: filteredUsers });
   };
 
